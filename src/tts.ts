e8fa3c0999dcc93d79a9979601fd1212a7884a65
CT-MarkLin <52391225+CT-MarkLin@users.noreply.github.com>
2025-07-22 01:51:10 +0800

    Update tts.ts

--- src/tts.ts
+++ src/tts.ts
@@ -4,8 +4,8 @@ import { EdgeTTS } from "edge-tts-universal";
 export const ttsApi = async (text: string) => {
   // Works in both Node.js and browsers (subject to CORS policy)
   const tts = new EdgeTTS(text, "zh-CN-XiaoxiaoNeural",{
-    rate: '+30%',
-    volume: '+30%',
+    rate: '+20%',
+    volume: '+20%',
   });
 
   const result = await tts.synthesize();
